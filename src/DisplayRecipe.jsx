dc53fb0c00c22db05e3ab5950f5804a0392d57d2
Arpita Saha <arpita.sahaa@gmail.com>
2021-06-11 17:50:12 -0400

    added key

--- src/DisplayRecipe.jsx
+++ src/DisplayRecipe.jsx
@@ -3,15 +3,15 @@ import React from "react";
 function DisplayRecipe({ recipes }) {
   return (
     <div>
-      {recipes.map((recipe) => {
+      {recipes.map((recipe, i) => {
         console.log(recipe);
         return (
-          <div>
+          <div key={i}>
             <img src={recipe.recipe.image} alt=""/>
             <h1>{recipe.recipe.label}</h1>
             <h3>{recipe.recipe.calories}</h3>
-            <div>{recipe.recipe.ingredientLines.map(el=>{
-                return <p>{el}</p>
+            <div>{recipe.recipe.ingredientLines.map((el, i)=>{
+                return <p key={i}>{el}</p>
             })}</div>
           </div>
         );
